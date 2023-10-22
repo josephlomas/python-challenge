9173b0eb32d66f5c65c8de0dc43430cc73dd6474
Joseph Lomas <josephlomasjr@gmail.com>
2023-10-22 15:39:36 -0400

    Pushing changes to GitHub

---
+++ PyBank/Main.py
@@ -0,0 +1,56 @@
+# Dependencies: Importing necessary modules
+import os
+import csv
+
+# File Path: Defining the path to the budget data CSV file
+current_working_dir = os.getcwd()
+budget_data_path = os.path.join(current_working_dir, 'Resources', 'budget_data.csv')
+
+# Read CSV: Opening and reading the budget data CSV file
+with open(budget_data_path, newline="") as budget_data:
+    csvreader = csv.reader(budget_data, delimiter=',')
+    csv_header = next(csvreader)
+
+# Lists for Data Storage: Creating lists to store months and profit/loss data
+    months = []
+    profit_losses = []
+
+# Loop through CSV Data: Reading each row to extract and process data
+    for row in csvreader:
+        months.append(row[0])
+        profit_losses.append(int(row[1]))
+
+# Calcualate Total Months
+    total_months = len(months)
+
+# Calculate Average Change
+    change = []
+
+    for x in range(1,len(profit_losses)):
+        change.append(int(profit_losses[x] - int(profit_losses[x-1])))
+        average_change = sum(change) / len(change)
+
+# Find Greatest Increase and Decrease in Profits
+    greatest_increase = max(change)
+    greatest_decrease = min(change)
+    max_month = months[change.index(max(change))]
+    min_month = months[change.index(min(change))]
+
+# Print Results to Terminal
+    print('Financial Analysis')
+    print("----------------------------")
+    print('Total Months: ' + str(total_months))
+    print('Net Total: ' + '($' + str(sum(profit_losses)) + ')')
+    print('Average Change: ' + (str(round(average_change,2))))
+    print('Greatest Increase in Profits: ' + str(months[change.index(max(change)) + 1]) + " " + '($' + str(greatest_increase) + ')')
+    print('Greatest Decrease in Profits: ' + str(months[change.index(min(change)) + 1]) + " " + '($' + str(greatest_decrease) + ')')
+
+# Output to a Text File
+file = open('PyBank.txt', 'w')
+file.write('Financial Analysis' + "\n")
+file.write("----------------------------" + "\n")
+file.write('Total MOnths: ' + str(total_months) + "\n")
+file.write('Net Total: ' + '($' + str(sum(profit_losses)) + ')' + "\n")
+file.write('Average Change: ' + (str(round(average_change,2))) + "\n")
+file.write('Greatest Increase in Profits: ' + str(months[change.index(max(change)) + 1]) + " " + '($' + str(greatest_increase) + ')'+ "\n")
+file.write('Greatest Decrease in Profits: ' + str(months[change.index(min(change)) + 1]) + " " + '($' + str(greatest_decrease) + ')'+ "\n")
---
+++ PyPoll/Main.py
@@ -0,0 +1,60 @@
+# Importing necessary depencies
+import os
+import csv
+
+# Set the file path for loading the election data
+current_directory = os.getcwd()
+election_data_path = os.path.join(current_directory, 'Resources', 'election_data.csv')
+
+# Initialize variables for vote counting candidate tracking
+total_votes = 0
+candidate_list = []
+candidate_votes = {}
+candidate_results = ""
+winner = ""
+win_count = 0
+win_percent = 0
+
+# Open and read CSV data to calculate total votes and votes per candidate
+with open(election_data_path, newline="") as election_data:
+    csvreader = csv.reader(election_data, delimiter=',')
+    csv_header = next(csvreader)
+
+# Iterate through each row in the CSV data
+    for row in csvreader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+
+# Calculate the percentage of votes each candidate received
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        candidate_results += f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n"
+
+# Determine the winner based on popular vote count
+        if candidate_votes[candidate_name] > win_count:
+            winner = candidate_name
+            win_count = candidate_votes[candidate_name]
+
+# Print election results to the terminal
+print('Election Results')
+print("-------------------------")
+print('Total Votes:'+ str(total_votes))
+print("-------------------------")
+print(candidate_results)
+print("-------------------------")
+print(f"Winner: " + winner)
+
+# Output results to a text file
+file = open('PyPoll.txt', 'w')
+file.write('Election Results' + "\n")
+file.write("-------------------------" + "\n")
+file.write('Total Votes:'+ str(total_votes) + "\n")
+file.write("-------------------------" + "\n")
+file.write(candidate_results + "\n")
+file.write("-------------------------" + "\n")
+file.write(f"Winner: " + winner + "\n")
